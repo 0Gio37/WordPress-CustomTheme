2e7182c8744e49437252bddeb79d1586de528b30
_GioLeone <gmessanges@gmail.com>
2021-07-12 20:21:34 +0200

    base

---
+++ 404.php
@@ -0,0 +1,3 @@
+<h1 class="error-404">Erreur lors du chargement de cette page</h1>
+
+<a href="#"><p>Retour à la page d'accueil</p>
---
+++ front-page.php
@@ -0,0 +1,4 @@
+<?php get_header();?>
+
+<h1>PAGE FRONT-PAGE</h1>
+<?php get_footer();?>
