d082439c4800e593a1ab87b6e6a60c37b2d5943b
_GioLeone <gmessanges@gmail.com>
2021-07-12 19:13:14 +0200

    init

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+
+<header>
+    <h2>header</h2>
+</header>
+
+
+
+
+
+
+
+
+<?php wp_body_open(); ?><?php
